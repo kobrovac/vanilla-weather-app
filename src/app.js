9aa9002c9a857e4681606788c1181535a8e280f4
kobrovac <120015883+kobrovac@users.noreply.github.com>
2023-01-12 14:52:52 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -35,8 +35,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "5aadb231294f844bcda7e81c8o0bct7f";
-let city = "Pula"
-let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+function search(city) {
+    let apiKey = "5aadb231294f844bcda7e81c8o0bct7f";
+    let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("Pula");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
