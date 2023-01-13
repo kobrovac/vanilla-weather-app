38b167a577540fc3600e6b98bad9366c8fb03104
kobrovac <120015883+kobrovac@users.noreply.github.com>
2023-01-13 11:05:19 +0100

    Added weather forecast HTML code in JS

--- src/app.js
+++ src/app.js
@@ -14,6 +14,36 @@ function formatDate(timestamp) {
     return `${day}, ${hours}:${minutes}`;
 }
 
+function displayForecast(){
+    let forecastElement = document.querySelector("#forecast");
+
+    let days = ["Fri", "Sat", "Sun", "Mon"];
+
+    let forecastHTML = `<div class="row">`
+    days.forEach(function(day) {
+        forecastHTML = forecastHTML + 
+    `
+        <div class="col-2">
+            <div class="weather-forecast-date">
+                ${day}
+            </div>
+                <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                alt="" width="42"/>
+                    <div class="weather-forecast-temperature">
+                       <span class="weather-forecast-temperature-max">
+                        18° </span>
+                        <span class="weather-forecast-temperature-min">
+                          12°
+                        </span>
+                    </div>
+        </div>
+           
+    `;
+   });
+    forecastHTML = forecastHTML + `</div>`
+    forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city")
@@ -80,3 +110,4 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
 search("Pula");
+displayForecast();
